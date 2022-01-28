d501308bc6cccab72023678101c3fe285857cc25
Andrew Walbran <qwandor@google.com>
2022-01-28 18:14:53 +0000

    Remove unused code.

--- src/config/defaults.rs
+++ src/config/defaults.rs
@@ -1,16 +1,5 @@
 use std::net::IpAddr;
 use std::net::Ipv4Addr;
-use url::Url;
-
-pub fn server_websocket_url() -> Url {
-    let url = format!("ws://localhost:{}", server_port());
-    Url::parse(&url).unwrap()
-}
-
-pub fn server_http_url() -> Url {
-    let url = format!("http://localhost:{}", server_port());
-    Url::parse(&url).unwrap()
-}
 
 pub const fn server_listen_address() -> IpAddr {
     IpAddr::V4(Ipv4Addr::LOCALHOST)
@@ -22,24 +11,4 @@ pub const fn server_port() -> u16 {
 
 pub const fn server_port_tls() -> u16 {
     6002
-}
-
-pub fn base_directories() -> xdg::BaseDirectories {
-    xdg::BaseDirectories::with_prefix("houseflow").unwrap()
-}
-
-pub fn config_home() -> std::path::PathBuf {
-    base_directories().get_config_home()
-}
-
-pub fn data_home() -> std::path::PathBuf {
-    base_directories().get_data_home()
-}
-
-pub fn clerk_path() -> std::path::PathBuf {
-    data_home().join("clerk.sled")
-}
-
-pub fn smtp_port() -> u16 {
-    587
 }
